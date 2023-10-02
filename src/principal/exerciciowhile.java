38481b0390b13d00271fb934e85768730fc6cce1
Roberta <robertakarolina.gomes@hotmail.com>
2023-10-02 16:14:59 -0300

    Exercicio while

---
+++ src/principal/exerciciowhile.java
@@ -0,0 +1,13 @@
+package principal;
+
+public class exerciciowhile {
+	public static void main(String[] args) {
+		int cc=0;
+		while(cc<10) {
+			System.out.println("Cambalhota" + cc);
+			cc++;
+			
+		}
+	}
+
+}
